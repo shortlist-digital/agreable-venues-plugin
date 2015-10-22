502824865ec566c4db260c5bbb89b6794bc859ce
garethfoote <gareth.foote@gmail.com>
2015-10-22 09:26:04 +0100

    Forceably removing topleft controls (hacky). Map move end event.

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,29 +6,63 @@ import { bindActionCreators } from 'redux';
 import { searchLocation } from '../actions';
 
 import Search from '../components/Search';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Map, MapComponent, Marker, Popup, TileLayer } from 'react-leaflet';
 
 class App extends Component {
+
+	constructor(props) {
+		super(props)
+    this.handleMoveEnd = this.handleMoveEnd.bind(this);
+	}
+
   static propTypes = {
     children: PropTypes.node
   }
 
-	_renderMap() {
-		const position = [51.505, -0.09];
-		return (
-			<Map className="venues__map" center={position} zoom={13}>
-				<TileLayer
-					url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-				/>
-				<Marker position={position}>
-					<Popup>
-						<span>A pretty CSS3 popup.<br/>Easily customizable.</span>
-					</Popup>
-				</Marker>
-			</Map>
-		);
-	}
+  handleMoveEnd(e) {
+    const map = this.refs.map.leafletElement
+		console.log(map.getBounds())
+  }
+
+  componentDidMount() {
+    const map = this.refs.map.leafletElement
+
+    // Manually remove top left zoom control because
+    // constructor param (zoomControl:false) is not working
+    // through react component.
+    const firstZoomControl = map._controlContainer.firstChild
+    firstZoomControl.parentNode.removeChild(firstZoomControl)
+
+    const zoomControl = L.control.zoom({
+      position: 'bottomleft'
+    });
+    map.addControl(zoomControl);
+  }
+
+  _renderMap() {
+    const position = [51.505, -0.09];
+
+    const map = (
+      <Map
+        ref="map"
+        zoomControl={"false"}
+        className="venues__map"
+        center={position}
+        onLeafletMoveend={this.handleMoveEnd}
+        zoom={15}>
+        <TileLayer
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+          </Popup>
+        </Marker>
+      </Map>
+    );
+    return map
+  }
 
   render() {
     const { searchLocationAction, currentSearch } = this.props;
@@ -37,22 +71,21 @@ class App extends Component {
       '/a-test-venue',
       '/another-test-venue',
     ].map(l =>
-      <p>
+      <div>
         <Link to={l}>{l}</Link>
-      </p>
+      </div>
     );
 
     return (
       <div>
-		{this._renderMap()}
-        <h1>Venues (Search term:{currentSearch})</h1>
+        {this._renderMap()}
         <Search
           searchTerm={currentSearch}
           onSearch={value =>
             searchLocationAction(value)
           } />
         {this.props.children}
-				<h3>Test links</h3>
+        <h3>Test links</h3>
         {links}
       </div>
     );
