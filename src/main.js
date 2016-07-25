837a6211df3751a26f9cb4ad16191059baf14674
Jasal Vadgama <jasal@blacklabelcreative.com>
2016-07-25 17:48:31 +0100

    adding forebase deps. creating working geo query.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import ReactDOM from 'react-dom'
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import { ReduxRouter } from 'redux-router'
+import firebase from 'firebase'
+import GeoFire from 'geofire'
 
 // Conditionally requiring store dependant on env. __PRODUCTION__ variable
 // defined in webpack conf.
@@ -22,6 +24,46 @@ if (__PRODUCTION__) {
   require('./createDevToolsWindow')(store);
 }
 
+// Initialize the Firebase SDK
+var firebaseConfig = {
+  apiKey: "AIzaSyBTJAJMSpkPjEPbWkrlphZ4FQ1bsJum0eY",
+  authDomain: "venues-database.firebaseapp.com",
+  databaseURL: "https://venues-database.firebaseio.com",
+  storageBucket: "venues-database.appspot.com",
+};
+firebase.initializeApp(firebaseConfig);
+var firebaseRef = firebase.database().ref('venues/_geofire');
+// firebaseRef.on('child_added', function(data) {
+//   console.log('child_added', data);
+// });
+var geoFire = new GeoFire(firebaseRef);
+// geoFire.get('Chuck Burger').then(function(location) {
+//   console.log(location);
+// });
+geoFire.set("TEST_Chucks", [51.5157496, -0.071967699999959]).then(function() {
+  console.log("Provided key has been added to GeoFire");
+}, function(error) {
+  console.log("Error: " + error);
+});
+geoFire.set("TEST_Park", [51.5082341, -0.14335329999994]).then(function() {
+  console.log("Provided key has been added to GeoFire");
+}, function(error) {
+  console.log("Error: " + error);
+});
+
+setTimeout(function() {
+  var geoQuery = geoFire.query({
+    center: [51.5283064, -0.3824583],
+    radius: 1000
+  });
+  geoQuery.on('key_entered', function(key, location, distance) {
+    console.log('key_entered', key, location, distance);
+  });
+  geoQuery.on('key_exited', function(key, location, distance) {
+    console.log('key_exited', key, location, distance);
+  });
+}, 1000);
+
 class Root extends Component {
   render() {
     const rootStyle = {
@@ -37,6 +79,6 @@ class Root extends Component {
   }
 }
 
-DOMReady(function() {
-  ReactDOM.render(<Root />, document.getElementById('agreable-venues'))
-})
+// DOMReady(function() {
+//   ReactDOM.render(<Root />, document.getElementById('agreable-venues'))
+// })
