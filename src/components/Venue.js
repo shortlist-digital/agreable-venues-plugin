a6c042739234e34fc65a25ed2229f57f00fd50ee
Jon Sherrard <jon@andthats.it>
2017-08-21 18:09:26 +0100

    updates to make sure third party optin is save

--- src/components/Venue.js
+++ src/components/Venue.js
@@ -81,12 +81,10 @@ class Venue extends Component {
 
     let form = e.currentTarget;
     let promotion = this.props.promotions.length > 0 ? this.props.promotions[0] : null;
-    console.log('promotion: ', promotion)
 
     // if there is an opt in check box
     if (promotion && promotion.promo_third_party === 1) {
       // set the opt in data
-      console.log('there is a promotion', this.checkOptIn)
       this.checkOptIn(form.querySelector('#third-party-optin'));
     }
 
@@ -101,6 +99,8 @@ class Venue extends Component {
       this.sendVoucher();
       this.saveEmail();
     }
+    const currentState = this.state
+
   }
 
   validateEmail(input) {
@@ -165,10 +165,9 @@ class Venue extends Component {
   }
 
   checkOptIn(input) {
-    console.log('input: ', input)
-    console.log(input.checked)
+    const inputStatus = input.checked
     // save state user opt in to third party
-    this.setState({thirdPartyOptIn: input.checked})
+    this.setState({thirdPartyOptIn: inputStatus})
   }
 
   sendVoucher() {
